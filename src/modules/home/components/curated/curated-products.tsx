ea8517c775511b35a2b43c9216363b7c30079db1
michaelbeihua <michaelbeihua@outlook.com>
2025-09-17 15:07:24 -0700

    in line blank span

--- src/modules/home/components/curated/curated-products.tsx
+++ src/modules/home/components/curated/curated-products.tsx
@@ -5,7 +5,8 @@ const CuratedProducts = () => {
   return (
     <section className="w-full px-4 md:px-8 py-10 select-none">
       <h2 className="text-2xl font-bold mb-6 text-left uppercase flex items-center" style={{ color: '#333' }}>
-        <span>Shop </span>
+        <span>Shop</span>
+        <span className="inline-block w-[0.25rem]"></span>
         <span className="text-lg font-medium tracking-wide text-black">REVETIR</span>
         <span>-curated wardrobes</span>
       </h2>
