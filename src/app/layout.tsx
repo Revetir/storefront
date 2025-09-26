27ccc09dc2a3456d5c2ed3fd8986f142a3b2db8e
michaelbeihua <michaelbeihua@outlook.com>
2025-09-25 17:59:05 -0700

    meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "styles/globals.css"
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
   title: "REVETIR - Luxury Fashion & Independent Designers",
-  description: "Discover luxury fashion and independent Asian designers. Free shipping & returns in the US.",
+  description: "Discover luxury fashion and independent Asian designers on sale. Free shipping & returns in the US.",
   icons: {
     icon: [
       { url: '/favicon.ico' },
@@ -37,6 +37,31 @@ export default function RootLayout(props: { children: React.ReactNode }) {
             gtag('config', 'G-PBQWGZXCMG');
           `}
         </Script>
+        
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1157756346271206');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1157756346271206&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body>
         <main className="relative">{props.children}</main>
