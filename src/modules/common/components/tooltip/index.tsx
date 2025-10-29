9ef7ddbb89650f488ef0022dafc8eb2e2dade845
michaelbeihua <michaelbeihua@outlook.com>
2025-10-29 11:57:55 -0700

    include original total in cart item for sale display in bag and fix tooltip

--- src/modules/common/components/tooltip/index.tsx
+++ src/modules/common/components/tooltip/index.tsx
@@ -61,7 +61,7 @@ const Tooltip: React.FC<TooltipProps> = ({ content, children }) => {
         </svg>
       </div>
       {isVisible && (
-        <div className="absolute z-50 left-full ml-2 top-1/2 -translate-y-1/2 px-3 py-2 text-xs text-gray-700 bg-white rounded-lg shadow-xl border border-gray-200 whitespace-normal w-64 md:w-auto md:max-w-md max-w-[calc(100vw-3rem)] animate-fadeIn before:content-[''] before:absolute before:right-full before:top-1/2 before:-translate-y-1/2 before:border-8 before:border-transparent before:border-r-white after:content-[''] after:absolute after:right-full after:top-1/2 after:-translate-y-1/2 after:border-8 after:border-transparent after:border-r-gray-200 after:-mr-px">
+        <div className="absolute z-50 left-full ml-2 top-1/2 -translate-y-1/2 px-3 py-2 text-xs text-gray-700 bg-white rounded-lg shadow-xl border border-gray-200 whitespace-normal w-64 md:w-auto md:max-w-lg animate-fadeIn before:content-[''] before:absolute before:right-full before:top-1/2 before:-translate-y-1/2 before:border-8 before:border-transparent before:border-r-white after:content-[''] after:absolute after:right-full after:top-1/2 after:-translate-y-1/2 after:border-8 after:border-transparent after:border-r-gray-200 after:-mr-px">
           <div className="text-left leading-snug normal-case">{content}</div>
         </div>
       )}
