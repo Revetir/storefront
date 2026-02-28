f2e272959e89df2615feca337903d6d493055739
michaelbeihua <michaelbeihua@outlook.com>
2026-02-28 12:17:28 -0800

    new Meta Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1157756346271206');
+            fbq('init', '806464238453144');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -88,7 +88,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
             height="1"
             width="1"
             style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=1157756346271206&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=806464238453144&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
