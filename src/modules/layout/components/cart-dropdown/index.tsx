8cb933d3fe730cd713ee619e7adee0ee2c312728
michaelbeihua <michaelbeihua@outlook.com>
2026-04-19 20:22:45 -0700

    fix cart dropdown flicker on load

--- src/modules/layout/components/cart-dropdown/index.tsx
+++ src/modules/layout/components/cart-dropdown/index.tsx
@@ -202,6 +202,9 @@ const CartDropdown = ({
   const itemRef = useRef<number>(totalItems || 0)
 
   const timedOpen = () => {
+    if (activeTimer) {
+      clearTimeout(activeTimer)
+    }
     open()
     const timer = setTimeout(close, 5000)
     setActiveTimer(timer)
@@ -667,7 +670,7 @@ const CartDropdown = ({
 
   // Open dropdown when cart item count changes, except on cart-like pages.
   useEffect(() => {
-    if (itemRef.current !== totalItems && !isCartLikePage) {
+    if (totalItems > itemRef.current && !isCartLikePage) {
       timedOpen()
     }
     itemRef.current = totalItems
@@ -712,12 +715,13 @@ const CartDropdown = ({
           <Transition
             show={cartDropdownOpen}
             as={Fragment}
+            unmount={false}
             enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
             leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
             <PopoverPanel
               static
